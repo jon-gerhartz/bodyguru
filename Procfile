4cbb3423437a3c253f73109d25d75f2c2c9939cd
Jonathan Gerhartz <jonathangerhartz@ip-192-168-1-156.us-east-2.compute.internal>
2023-11-20 23:49:06 -0500

    added migration file to procfile. Added DB password to extensions and conn string

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python run_db_init.py
 web: gunicorn -b 0.0.0.0:$PORT app:app
--- extensions.py
+++ extensions.py
@@ -7,10 +7,12 @@ DB_USER = os.getenv("DB_USER")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+DB_PW = os.getenv("DB_PW")
 
 conn = pg8000.connect(
     user=DB_USER,
     host=DB_HOST,
     port=DB_PORT,
-    database=DB_NAME
+    database=DB_NAME,
+    password=DB_PW
 )
